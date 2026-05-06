c55ae4d5140456509bf935129d4cd4e70ad4683f
cccc1808 <cccc1808@>
2026-05-06 21:25:24 +0400

    Комментарий.

--- src/cccc1808.ProcessEngine.Model/Abstract/WakeupModule/Dto/WakeupStateEnum.cs
+++ src/cccc1808.ProcessEngine.Model/Abstract/WakeupModule/Dto/WakeupStateEnum.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using cccc1808.ProcessEngine.Model.Abstract.TriggerModule.Components;
 using cccc1808.ProcessEngine.Model.Abstract.WakeupModule.Handlers;
+using cccc1808.ProcessEngine.Model.Abstract.WakeupModule.Services;
 
 namespace cccc1808.ProcessEngine.Model.Abstract.WakeupModule.Dto
 {
@@ -13,13 +14,17 @@ namespace cccc1808.ProcessEngine.Model.Abstract.WakeupModule.Dto
     {
         /// <summary>
         /// Механизм check wakeup не используется.
+        /// * Все триггеры должны блокировать процесс напрямую.
         /// </summary>
         NoWakeup,
 
         /// <summary>
         /// Механизм check wakeup используется.
         /// * Задействовано отдельная таблица для обработки concurrency.
-        /// Сначала береться блокировка, потом проверяется условие <see cref="IWakeupCheckHandler{TId}"/>.
+        /// Сначала береться блокировка, потом проверяется условие <see cref="IWakeupCheckHandler{TId}"/>,
+        /// это гарантирует что сигнал не будет потерян (он либо уже записан до блокировки, либо оповещение о нем поступит после блокировки - пробуждение от триггера).
+        /// * Позволяет триггеру не конкурировать за блокировку процесса, если он выполняется, береться только share блокировка на WakeupLockTable.
+        /// * Все триггеры должны вызывать <see cref="IWakeupService{TId}.WakeupProcessHandlerAsync(ICollection{TId}, bool, CancellationToken)"/> (кроме триггера ошибки).
         /// </summary>
         CheckWakeupWithLock,
 
@@ -27,8 +32,10 @@ namespace cccc1808.ProcessEngine.Model.Abstract.WakeupModule.Dto
         /// Механизм check wakeup используется.
         /// * Отдельная таблица не используется (низкий или отсуствие concurrency).
         /// Условие <see cref="IWakeupCheckHandler{TId}"/> проверяется без дополнительной блокировки.
-        /// * Может использовать для триггера <see cref="ITriggerComponent.TriggerKind.SimpleStream"/> т.к. 
-        /// тригер знает о состоянии стрима и конкуренции не будет.
+        /// * Может использовать в ситуации когда используется
+        /// 1 <see cref="ITriggerComponent.TriggerKind.SimpleStream"/> или <see cref="ITriggerComponent.TriggerKind.OffsetStream"/> 
+        /// т.к. тригер знает о состоянии стрима и конкуренции и потери сигнала не будет.
+        /// * Все триггеры должны блокировать процесс напрямую.
         /// </summary>
         CheckWakeupWithoutLock,        
     }
