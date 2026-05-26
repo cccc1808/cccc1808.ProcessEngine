63287fb990f199087dd1d6c4720b435b07a153af
cccc1808 <cccc1808@>
2026-05-26 13:41:20 +0400

    * Поправленна упорядоченность ITriggerEvent в TriggerEventRaiserAfterTransactionCompleteDecorator.

--- src/cccc1808.ProcessEngine.Model/Implementation/TriggerModule/Services/Events/TriggerEventRaiserAfterTransactionCompleteDecorator.cs
+++ src/cccc1808.ProcessEngine.Model/Implementation/TriggerModule/Services/Events/TriggerEventRaiserAfterTransactionCompleteDecorator.cs
@@ -16,9 +16,11 @@ namespace cccc1808.ProcessEngine.Model.Implementation.TriggerModule.Services.Eve
     {
         private readonly ITriggerEventRaiser<TId> _source;
         private readonly ITransactionManager _transactionManager;
-        private readonly IIsolationService _isolationService;
+        private readonly IIsolationService _isolationService;        
         
-        private readonly Dictionary<Guid, ICollection<ITriggerEventRaiser<TId>.RaiseContainer>> _sendBuffer;
+        private readonly Dictionary<int, ICollection<ITriggerEventRaiser<TId>.RaiseContainer>> _sendBuffer;
+
+        private int RaiseCounter { get; set; }
 
         private bool HandlerRegistered { get; set; }
 
@@ -30,7 +32,7 @@ namespace cccc1808.ProcessEngine.Model.Implementation.TriggerModule.Services.Eve
             _source = source;
             _transactionManager = transactionManager;            
             _isolationService = isolationService;
-            _sendBuffer = new Dictionary<Guid, ICollection<ITriggerEventRaiser<TId>.RaiseContainer>>();
+            _sendBuffer = new Dictionary<int, ICollection<ITriggerEventRaiser<TId>.RaiseContainer>>(10);
         }
 
         public ValueTask RaiseAsync(
@@ -51,7 +53,7 @@ namespace cccc1808.ProcessEngine.Model.Implementation.TriggerModule.Services.Eve
                         // Игнорируем cancelation token, чтобы выполнить отправку, даже если сервис останавливается.
                         // Если будет gracefull shutdown, то событие будет опубликовано, иначе событие потеряется.
                         await _source.RaiseAsync(
-                            _sendBuffer.Values.SelectMany(e => e).ToArray(),
+                            _sendBuffer.OrderBy(e => e.Key).SelectMany(e => e.Value).ToArray(),
                             default);
                     },
                     roolbackHandler: CompensateTransactionHandler
@@ -60,8 +62,8 @@ namespace cccc1808.ProcessEngine.Model.Implementation.TriggerModule.Services.Eve
                 HandlerRegistered = true;
             }
 
-            var key = Guid.NewGuid();
-            _sendBuffer.Add(key, events);
+            var number = RaiseCounter++;
+            _sendBuffer.Add(number, events);
             
             // Если находимся в scope изоляции, то регистрируем событие на случай компенсации scope.
             if (_isolationService.InScope)
@@ -70,7 +72,7 @@ namespace cccc1808.ProcessEngine.Model.Implementation.TriggerModule.Services.Eve
                 _isolationService.RegisterManualCompensate(
                     (t) => 
                     {
-                        _sendBuffer.Remove(key);
+                        _sendBuffer.Remove(number);
                         return ValueTask.CompletedTask;
                     }
                     );
