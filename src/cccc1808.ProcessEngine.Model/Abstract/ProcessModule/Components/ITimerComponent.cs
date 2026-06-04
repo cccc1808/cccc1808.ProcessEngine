b8dcb18d3eef9939c99adac17fcce411cd18c771
cccc1808 <cccc1808@>
2026-06-04 15:52:29 +0400

    Заготовка ITimerComponent.

---
+++ src/cccc1808.ProcessEngine.Model/Abstract/ProcessModule/Components/ITimerComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cccc1808.ProcessEngine.Model.Abstract.ProcessModule.Components
+{
+    /// <summary>
+    /// Компонент для взаимодействия с таймера процесса.
+    /// В транзакции создается timer trigger и данные о таймере записываются в процесс <see cref="CreateTimer(string, DateTimeOffset)"/>.
+    /// Позволяет проверить наличие активированных таймеров.
+    /// </summary>
+    public interface ITimerComponent
+    {
+        void CreateTimer(string key, DateTimeOffset date);
+
+        bool TryGetTimer(string key, out DateTimeOffset date);
+
+        void RemoveTimer(string key);
+
+        /// <summary>
+        /// Есть сработавшие таймере (нужно обработать и либо обновить дату, либо удалить таймер).
+        /// </summary>
+        /// <param name="now"></param>
+        /// <param name="timers">Перечень активированных таймеров.</param>
+        /// <returns></returns>
+        bool TryGetActivatedTimers(DateTimeOffset now, out IDictionary<string, DateTimeOffset> timers);
+    }
+}
---
+++ src/cccc1808.ProcessEngine.Model/Implementation/ProcessModule/Components/InMemoryTimerComponent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using cccc1808.ProcessEngine.Model.Abstract.ProcessModule.Components;
+
+namespace cccc1808.ProcessEngine.Model.Implementation.ProcessModule.Components
+{
+    public class InMemoryTimerComponent : ITimerComponent
+    {
+        private readonly Dictionary<string, DateTimeOffset> _data;
+
+        public InMemoryTimerComponent(Dictionary<string, DateTimeOffset> data)
+        {
+            _data = data;
+        }
+
+        public void CreateTimer(string key, DateTimeOffset date)
+        {
+            _data.Add(key, date);
+        }
+
+        public void RemoveTimer(string key)
+        {
+            _data.Remove(key);
+        }
+
+        public bool TryGetActivatedTimers(
+            DateTimeOffset now,
+            out IDictionary<string, DateTimeOffset> timers)
+        {
+            timers = _data
+                .Where(e => e.Value > now)
+                .ToDictionary();
+            return timers.Any();
+        }
+
+        public bool TryGetTimer(string key, out DateTimeOffset date)
+        {
+            return _data.TryGetValue(key, out date);
+        }
+    }
+}
